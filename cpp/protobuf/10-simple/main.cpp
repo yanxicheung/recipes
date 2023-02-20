16e44007d75f394b1943f31fc342b8b1c6fb2241
daniel_thinkpad <1206767659@qq.com>
2023-02-20 14:46:36 +0800

    add protobuf simple demo

---
+++ cpp/protobuf/10-simple/main.cpp
@@ -0,0 +1,121 @@
+#include "addressbook.pb.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+using namespace daniel::tutorial;
+
+void createAddressBook(AddressBook& book)
+{
+	Person* p1 = book.add_person();
+	// 自己定义的复杂嵌套消息不能够通过简单的set_来赋值，可采取set_allocated和mutable_两种方式
+	BasicInfo* pBasic = p1->mutable_basicinfo();
+	pBasic->set_name("daniel");
+	pBasic->set_id(151040023);
+	pBasic->set_email("daniel@163.com");
+
+	Person_PhoneNumber* phone1 = p1->add_phone();
+	phone1->set_type(Person_PhoneType::Person_PhoneType_MOBILE);
+	phone1->set_number("17654387654");
+
+	Person_PhoneNumber* phone2 = p1->add_phone();
+	phone2->set_type(Person_PhoneType::Person_PhoneType_HOME);
+	phone2->set_number("0512-87655432");
+
+	// 调试打印;
+	cout << book.DebugString() << endl;
+}
+
+void createAddressBook1(AddressBook& book)
+{
+	Person* p1 = book.add_person();
+	BasicInfo* pBasic = p1->mutable_basicinfo();
+	pBasic->set_name("zpp");
+	pBasic->set_id(11041531);
+	pBasic->set_email("zpp@163.com");
+
+	Person_PhoneNumber phone1;
+	phone1.set_number("17654387675");
+	phone1.set_type(Person_PhoneType::Person_PhoneType_MOBILE);
+
+	Person_PhoneNumber phone2;
+	phone2.set_number("0513-87655431");
+	phone2.set_type(Person_PhoneType::Person_PhoneType_HOME);
+
+	vector<Person_PhoneNumber> phones;
+	phones.push_back(phone1);
+	phones.push_back(phone2);
+	p1->mutable_phone()->CopyFrom({ phones.begin(), phones.end() });
+
+	// 调试打印;
+	cout << book.DebugString() << endl;
+}
+
+
+void fileCodec(const AddressBook& book)
+{
+	cout << "================fileCodec================" << endl;
+	// 序列化到文件
+	fstream output("addressBook", ios::out | ios::trunc | ios::binary);
+	book.SerializePartialToOstream(&output);
+	output.close();
+
+	// 从文件读取二进制流,反序列化
+	daniel::tutorial::AddressBook  book1;
+	fstream input("addressBook", ios::in | ios::binary);
+	book1.ParseFromIstream(&input);
+	for (int i = 0; i < book1.person_size(); ++i)
+	{
+		const Person& person = book1.person(i);
+		const BasicInfo& basicInfo = person.basicinfo();
+		cout << "id " << basicInfo.id() << endl;
+		cout << "email " << basicInfo.email() << endl;
+		cout << "name " << basicInfo.name() << endl;
+		for (int j = 0; j < person.phone_size(); ++j)
+		{
+			const Person_PhoneNumber& phone = person.phone(j);
+			cout << "type " << phone.type() << " number " << phone.number() << endl;
+		}
+		cout << endl;
+	}
+}
+
+void stringCodec(const AddressBook& book)
+{
+	cout << "================stringCodec================" << endl;
+	// 序列化到string,这里string当作一个容器来使用,存储二进制字节流
+	string data;
+	book.SerializeToString(&data);
+
+	// 从string读取二进制流,反序列化
+	daniel::tutorial::AddressBook  book1;
+	book1.ParseFromString(data);
+	for (int i = 0; i < book1.person_size(); ++i)
+	{
+		const Person& person = book1.person(i);
+		const BasicInfo& basicInfo = person.basicinfo();
+		cout << "id " << basicInfo.id() << endl;
+		cout << "email " << basicInfo.email() << endl;
+		cout << "name " << basicInfo.name() << endl;
+		for (int j = 0; j < person.phone_size(); ++j)
+		{
+			const Person_PhoneNumber& phone = person.phone(j);
+			cout << "type " << phone.type() << " number " << phone.number() << endl;
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	daniel::tutorial::AddressBook  book;
+	createAddressBook(book);
+	fileCodec(book);
+	stringCodec(book);
+
+	book.Clear();
+	createAddressBook1(book);
+	fileCodec(book);
+	stringCodec(book);
+	return 0;
+}
---
+++ cpp/protobuf/Protobuf.md
@@ -0,0 +1,98 @@
+# 序列化原理
+
+
+
+# 反射
+
+
+
+# protobuf
+
+## 安装
+
+介绍安装`protocol buffer`的编译器:`protoc`
+
+（1）下载 `protobuf`安装包:
+
+```bash
+git clone https://github.com/protocolbuffers/protobuf.git
+```
+
+（2）安装依赖:
+
+```bash
+sudo apt-get install autoconf automake libtool curl make g++ unzip libffi-dev -y
+```
+
+（3）安装`protoc`:
+
+```bash
+cd protobuf/
+./autogen.sh
+./configure
+make
+sudo make install
+sudo ldconfig  // 刷新共享库，很重要的一步
+```
+
+
+（4）测试一下是否安装成功 :
+
+```bash
+ protoc --version
+```
+
+## 编译：
+
+介绍基于`.proto`文件运行`protocol buffer`编译器`protoc`，生成对应的源文件，运行的命令如下所示：
+
+```bash
+protoc --proto_path=IMPORT_PATH --cpp_out=DST_DIR --java_out=DST_DIR --python_out=DST_DIR path/to/file.proto
+```
+
+`IMPORT_PATH`声明了一个`.proto`文件所在的具体目录。如果忽略该值，则使用当前目录。如果有多个目录则可以 对`--proto_path` 写多次，它们将会顺序的被访问并执行导入。`-I=IMPORT_PATH`是它的简化形式。
+
+
+
+## 关键字：
+
+### repeated
+
+对于`repeated`变量，提供了其它一些特殊的方法：
+
+- _size方法：返回repeated field’s
+- 通过下脚标访问其中的数组成员组
+- 通过下脚标返回其中的成员的mutable_的方法
+- add_方法：增加一个成员。
+
+如果proto结构体的变量是基础变量，比如`int`、`string`、`枚举`等等，那么set的时候直接调用`set_xxx`即可。
+
+如果变量是**自定义类型(message)**，那么C++的生成代码中，就没有`set_xxx`函数名，取而代之的是三个函数名：
+
+- **set_allocated_xxx()**
+- **release_xxx()**
+- **mutable_xxx()**
+
+使用`set_allocated_xxx()`来设置变量的时候，变量不能是普通栈内存数据，必须是手动new出来的指针，至于何时delete，就不需要调用者关心了，protobuf内部会自动delete掉通过`set_allocated_xxx()`设置的内存；
+
+`release_xxx()`是用来取消之前由`set_allocated_xxx()`设置的数据，调用`release_xxx()`以后，protobuf内部就不会自动去delete这个数据了；
+
+`mutable_xxx()`是用来返回之前`set_allocated_xxx()`设置的数据的const指针，方便你临时读取这个数据的同时，protobuf内部继续自动管理(delete)该数据。
+
+### oneof
+
+
+
+### any
+
+
+
+## 实践
+
+示例基于`protobuf 3.13.0`版本
+
+
+
+# ref
+
+4. [C++序列化工具最佳实践](https://www.jianshu.com/p/50065ff82be3)
